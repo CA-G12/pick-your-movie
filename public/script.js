2b14177f94a563065115e8763522a33564fbff90
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-13 00:22:14 +0300

    Edited teh script file, moved fetch function to get rid of the require error on the borwser, and edited the callback function that is add to teh event listener close #8

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-const fetch = require('./fetch');
-
 const searchInput = document.getElementById('search');
 const autocomplete = document.querySelector('.autocomplete');
 
@@ -7,12 +5,30 @@ const manipulateDOM = (data) => {
   autocomplete.innerHTML = '';
   data.forEach((title) => {
     const titlePara = document.createElement('p');
-    titlePara.className.add('title');
+    titlePara.classList.add('title');
     titlePara.textContent = title;
     autocomplete.appendChild(titlePara);
   });
 };
 
+const fetch = (method, uri, cb) => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      cb(data);
+    }
+  };
+
+  xhr.open(method, uri, true);
+  xhr.send();
+};
+
 searchInput.addEventListener('keyup', (event) => {
-  fetch('POST', '/autocomplete', manipulateDOM, event.target.value);
+  const uri = `/autocomplete?data=${encodeURIComponent(event.target.value)}`;
+  if (event.target.value === '') autocomplete.innerHTML = '';
+  if (event.target.value !== '') {
+    fetch('GET', uri, manipulateDOM);
+  }
 });
